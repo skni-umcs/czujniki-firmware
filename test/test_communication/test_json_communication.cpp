cb6a0b261fa61b5757d3877462961de803fd9194
Osariusz <osariusz@interia.pl>
2024-09-07 13:45:31 +0200

    Fix unit tests

--- test/test_communication/test_json_communication.cpp
+++ test/test_communication/test_json_communication.cpp
@@ -5,6 +5,8 @@
 #include <exchange/transmits/wifiTransmit.h>
 #include <iostream>
 #include <memory>
+#include <Adafruit_I2CDevice.h>
+#include <SPI.h>
 
 std::shared_ptr<JsonCommunication> jsonCommunication;
 std::unique_ptr<JsonTransmit> jsonTransmit;
@@ -12,15 +14,12 @@ std::unique_ptr<JsonTransmit> jsonTransmit;
 void setup() {
     jsonCommunication = JsonCommunication::create();
     jsonTransmit = std::unique_ptr<JsonTransmit>(new WifiTransmit());
+UNITY_BEGIN();
+    jsonCommunication->subscribe(std::move(jsonTransmit));
+    TEST_ASSERT_TRUE(jsonCommunication->getTransmitTo().size() == 1);
+    UNITY_END();
 }
 
 void loop() {
     // clean stuff up here
-}
-
-int main( int argc, char **argv) {
-    UNITY_BEGIN();
-    jsonCommunication->subscribe(std::move(jsonTransmit));
-    TEST_ASSERT_TRUE(jsonCommunication->getTransmitTo().size() == 1);
-    UNITY_END();
 }
