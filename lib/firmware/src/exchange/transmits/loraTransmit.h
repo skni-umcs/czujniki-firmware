1461f525b66afc12cdf8cc913ad83c8834af5e88
osariusz <osariusz@interia.pl>
2024-11-24 13:10:15 +0100

    Cleanup

--- lib/firmware/src/exchange/transmits/loraTransmit.h
+++ lib/firmware/src/exchange/transmits/loraTransmit.h
@@ -7,9 +7,15 @@
 #include "jsonTransmit.h"
 #include <LoRa_E220.h>
 
+const int TX_PIN = 17;
+const int RX_PIN = 16;
+const int AUX_PIN = 18;
+const int M0_PIN = 21;
+const int M1_PIN = 19;
+
 class LoraTransmit : public JsonTransmit
 {
-    LoRa_E220 e220ttl = LoRa_E220(17, 16, &Serial2, 18, 21, 19, UART_BPS_RATE_9600);
+    LoRa_E220 e220ttl = LoRa_E220(TX_PIN, RX_PIN, &Serial2, AUX_PIN, M0_PIN, M1_PIN, UART_BPS_RATE_9600);
     public:
         static std::shared_ptr<LoraTransmit> create();
         void setup();
