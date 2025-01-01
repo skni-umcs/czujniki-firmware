6c8052685fe3a975e004774014417f3065e6ba17
osariusz <osariusz@interia.pl>
2025-01-01 22:49:38 +0100

    Intro to packetsg

--- lib/firmware/src/exchange/transmits/loraTransmit.cpp
+++ lib/firmware/src/exchange/transmits/loraTransmit.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <LoRa_E220.h>
 #include "utils/stringUtils.h"
+#include "framing.h"
 
 void printParameters(struct Configuration configuration);
 
@@ -42,9 +43,13 @@ std::shared_ptr<LoraTransmit> LoraTransmit::create() {
 OperationResult LoraTransmit::send(std::string message, moduleAddress destinationNode) {
 	Serial.println("Hi, I'm going to send message!");
 
+	const int payloadSize = 200;
+	bfs::FrameEncoder<payloadSize> encoder;
+	encoder.Write(reinterpret_cast<const uint8_t*>(&message[0]), sizeof(message));
+
 	// Send message
 	//ResponseStatus rs = e220ttl.sendBroadcastFixedMessage(23, "Hello, world?");
-	ResponseStatus rs = e220ttl.sendBroadcastFixedMessage(23, "Siema świat");
+	ResponseStatus rs = e220ttl.sendBroadcastFixedMessage(23, encoder.data(), encoder.size());
 	// Check If there is some problem of succesfully send
 	Serial.println(rs.getResponseDescription());
     return OperationResult::SUCCESS;
