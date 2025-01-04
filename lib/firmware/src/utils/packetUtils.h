18831e691d953ac3bfe323859d918b8ac504eec8
osariusz <osariusz@interia.pl>
2025-01-04 11:35:15 +0100

    Cleanup

--- lib/firmware/src/utils/packetUtils.h
+++ lib/firmware/src/utils/packetUtils.h
@@ -11,8 +11,6 @@ enum class TransmissionCode : char {
     MESSAGE = 'm'
 };
 
-std::string transmissionCodeFromEnum(TransmissionCode transmissionCode);
-
 class PacketMessage {
     TransmissionCode type;
     std::string message;
@@ -24,6 +22,7 @@ class PacketMessage {
 
 template<typename T>
 std::string toHexString(T address);
+std::string transmissionCodeFromEnum(TransmissionCode transmissionCode);
 std::string createPacket(std::string message, moduleAddress senderNode, moduleAddress destinationNode);
 moduleAddress getDestinationAddress(std::string packet);
 std::string getPacketMessage(std::string packet);
