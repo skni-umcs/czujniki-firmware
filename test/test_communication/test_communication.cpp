94db149267460d6f829224ec0befda8d7996a52b
osariusz <osariusz@interia.pl>
2025-01-12 17:29:17 +0100

    Passthrough tests

--- test/test_communication/test_communication.cpp
+++ test/test_communication/test_communication.cpp
@@ -8,6 +8,7 @@
 #include <memory>
 #include <Adafruit_I2CDevice.h>
 #include <SPI.h>
+#include <exchange/communications/passthroughCommunication.h>
 
 void test_subscription(std::shared_ptr<JsonCommunication> jsonCommunication, std::unique_ptr<JsonTransmit> jsonTransmit) {
     jsonCommunication->subscribe(std::move(jsonTransmit));
@@ -28,10 +29,21 @@ void test_lora_subscription() {
     );
 }
 
+auto passthroughCommunication = PassthroughCommunication::create();
+
+void test_wrong_message_type() {
+    auto senders = std::vector<moduleAddress>{55};
+    auto rssi = std::vector<std::string>{};
+    auto hopLimit = 100;
+    auto message = std::shared_ptr<GeneratedMessage>(new GeneratedMessage(senders, 1, "test", rssi, hopLimit));
+    TEST_ASSERT_EQUAL(OperationResult::ERROR, passthroughCommunication->getNotified(message));
+}
+
 void setup() {
     UNITY_BEGIN();
     RUN_TEST(test_wifi_subscription);
     RUN_TEST(test_lora_subscription);
+    RUN_TEST(test_wrong_message_type);
     UNITY_END();
 }
 
