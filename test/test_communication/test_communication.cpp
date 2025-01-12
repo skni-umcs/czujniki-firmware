a38971bfe03f9c6e0dd09e44bdc1c2dd30a41619
osariusz <osariusz@interia.pl>
2025-01-12 17:38:26 +0100

    Passthrough tests

--- test/test_communication/test_communication.cpp
+++ test/test_communication/test_communication.cpp
@@ -39,11 +39,23 @@ void test_wrong_message_type() {
     TEST_ASSERT_EQUAL(OperationResult::ERROR, passthroughCommunication->getNotified(message));
 }
 
+void test_no_message_in_set() {
+    auto senders = std::vector<moduleAddress>{55, 133};
+    auto hopLimit = 2;
+    auto rssi = std::vector<std::string>{"RSSI37"};
+    auto snr = -90;
+    int currentRssi = 250;
+    auto message = std::shared_ptr<LoraMessage>(new LoraMessage(senders, 1, "test", rssi, hopLimit, currentRssi, snr));
+    std::set<std::shared_ptr<LoraMessage>> set;
+    passthroughCommunication->removeMessage(set, message);
+}
+
 void setup() {
     UNITY_BEGIN();
     RUN_TEST(test_wifi_subscription);
     RUN_TEST(test_lora_subscription);
     RUN_TEST(test_wrong_message_type);
+    RUN_TEST(test_no_message_in_set);
     UNITY_END();
 }
 
