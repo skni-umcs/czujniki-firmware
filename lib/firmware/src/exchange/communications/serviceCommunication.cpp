9d73de0e6b858dd28944c1d630c1f72e6bdb9264
Osariusz <osariusz@interia.pl>
2025-02-26 22:50:44 +0100

    Add time sync timeout

--- lib/firmware/src/exchange/communications/serviceCommunication.cpp
+++ lib/firmware/src/exchange/communications/serviceCommunication.cpp
@@ -8,6 +8,8 @@
 #include "time/timeConstants.h"
 #include <time/timerUpdate.h>
 
+static int ASK_TIMEOUT_MS = 60000;
+
 std::shared_ptr<ServiceCommunication> ServiceCommunication::create() {
     auto s = new ServiceCommunication();
     return std::shared_ptr<ServiceCommunication>{s};
@@ -50,11 +52,16 @@ void ServiceCommunication::sendResetReason() {
 }
 
 OperationResult ServiceCommunication::askForTime() {
-    PacketMessage packetMessage = PacketMessage(TransmissionCode::TIME_SYNCHRONIZATION, "t");
     lastAskTime = rtc.getEpoch();
-    Serial.println(lastAskTime);
-    Serial.println("lastAskTime");
-    this->transmit(packetMessage.getJson(), SERVER_ADDRESS);
+    while(lastAskTime != DIDNT_ASK) {
+        lastAskTime = rtc.getEpoch();
+        PacketMessage packetMessage = PacketMessage(TransmissionCode::TIME_SYNCHRONIZATION, "t");
+        Serial.println(lastAskTime);
+        Serial.println("lastAskTime");
+        this->transmit(packetMessage.getJson(), SERVER_ADDRESS);
+        delay(ASK_TIMEOUT_MS);
+        Serial.println("Ask for time timeout, checking if time updated");
+    }
     return OperationResult::SUCCESS;
 }
 
@@ -62,11 +69,13 @@ OperationResult ServiceCommunication::updateTime(unsigned long serverTime) {
     if(lastAskTime == DIDNT_ASK) {
         return OperationResult::OPERATION_IGNORED;
     }
+
     unsigned long currentTime = rtc.getEpoch();
     unsigned long RTT = currentTime-lastAskTime;
+    Serial.printf("I waited from %i to %i with RTT of %i, new time will be %i\n", lastAskTime, currentTime, RTT, serverTime+RTT/2);
     TimerUpdate::setTime(serverTime+RTT/2);
     lastAskTime = DIDNT_ASK;
-    Serial.printf("Time updated to: %lu\n", rtc.getEpoch());
+    Serial.printf("Current time after update: %lu\n", rtc.getEpoch());
     return OperationResult::SUCCESS;
 }
 
