17b352f28a74d9d85cf444c7591f974fdaf84376
osariusz <osariusz@interia.pl>
2025-03-30 17:21:46 +0200

    Cleanup

--- lib/firmware/src/exchange/communications/UpdateCommunication.cpp
+++ lib/firmware/src/exchange/communications/UpdateCommunication.cpp
@@ -4,13 +4,16 @@
 #include <utils/otherUtils.h>
 #include "utils/addressHandler.h"
 #include <Arduino_ESP32_OTA.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <Update.h>
 
 std::shared_ptr<UpdateCommunication> UpdateCommunication::create() {
     auto s = new UpdateCommunication();
     return std::shared_ptr<UpdateCommunication>{s};
 }
 
-static char const OTA_FILE_LOCATION[] = "https://raw.githubusercontent.com/Osariusz/ota_test2/main/firmware.ota";
+static char const OTA_FILE_LOCATION[] = "http://192.168.1.18:8000/firmware.bin";
 
 OperationResult UpdateCommunication::getNotified(std::shared_ptr<Message> message) {
     if(message->getIsPacketCorrect() && message->getContent() == "u") {
@@ -19,19 +22,14 @@ OperationResult UpdateCommunication::getNotified(std::shared_ptr<Message> messag
     }
     return OperationResult::SUCCESS;
 }
-#include <WiFi.h>
-#include <HTTPClient.h>
-#include <Update.h>
 
 OperationResult UpdateCommunication::update() {
-    const char* firmware_url = "http://192.168.1.18:8000/firmware.bin";
-
     Serial.println("Starting OTA update from local server...");
 
     WiFiClient client;
     HTTPClient http;
 
-    http.begin(client, firmware_url);
+    http.begin(client, OTA_FILE_LOCATION);
     int httpCode = http.GET();
 
     if (httpCode == HTTP_CODE_OK) {
