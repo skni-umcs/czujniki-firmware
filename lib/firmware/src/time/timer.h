e6adff8b4158c752105de94e058a62e683c9878c
osariusz <osariusz@interia.pl>
2025-05-22 12:38:32 +0200

    todo

--- lib/firmware/src/time/timer.h
+++ lib/firmware/src/time/timer.h
@@ -13,6 +13,7 @@ typedef std::function<bool()> timerConditionType;
 class Timer : public std::enable_shared_from_this<Timer>
 {
     int periodMs = 60000;
+    //TODO: remove update skips
     bool recentlyUpdated = false;
     executeFunctionType executeFunction = nullptr;
     timerConditionType timerCondition = nullptr;
